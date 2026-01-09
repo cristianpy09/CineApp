be9026f783e3fbea6b0b59571efa5efbb207ae96
cristianpy09 <cristiancelismoreno@gmail.com>
2026-01-09 08:54:03 -0500

    header links added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,21 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
     <nav className='flex justify-center h-15 bg-black  '>
         <ul className='flex space-x-5'>
+            <Link href={"/"} >
             <li>
                 Inicio
             </li>
+            </Link>
+            <Link href={"/peliculas"}>
             <li>
                 Peliculas
             </li>
+            </Link>
+            <Link href={"/series"} >
             <li>
                 Series
             </li>
+            </Link>
+
+            <Link href={"/anime"}>
             <li>
                 Anime
             </li>
+            </Link>
             <li>
                <input placeholder='Buscar' type="text" name="" id="" />
             </li>
---
+++ app/series/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1>pagina de solo series</h1>
+    </div>
+  )
+}
